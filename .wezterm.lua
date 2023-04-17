43d0f42acdecd5d81719a1aa0ea668e8c446f90a
hendrik.loewe <hendrikloewe3@gmail.com>
2023-04-17 19:03:04 +0200

    Add wezterm config

---
+++ .wezterm.lua
@@ -0,0 +1,69 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+config.color_scheme = 'Tokyo Night (Gogh)'
+config.hide_tab_bar_if_only_one_tab = true
+
+config.window_background_image = '/home/h.loewe/Pictures/Wallpaper/butterfly.jpg'
+config.window_background_image_hsb = {
+  brightness = 0.0175,
+  hue = 1,
+  saturation = 1,
+}
+
+config.window_padding = {
+  left = 0,
+  right = 0,
+  top = 0,
+  bottom = 0,
+}
+
+config.keys = {
+  {
+    key = 'e',
+    mods = 'CTRL|SHIFT',
+    action = wezterm.action.SplitHorizontal{ domain = 'CurrentPaneDomain' },
+  },
+  {
+    key = 'o',
+    mods = 'CTRL|SHIFT',
+    action = wezterm.action.SplitVertical{ domain = 'CurrentPaneDomain' },
+  },
+  {
+    key = 'LeftArrow',
+    mods = 'ALT',
+    action = wezterm.action.ActivatePaneDirection 'Left',
+  },
+  {
+    key = 'RightArrow',
+    mods = 'ALT',
+    action = wezterm.action.ActivatePaneDirection 'Right',
+  },
+  {
+    key = 'UpArrow',
+    mods = 'ALT',
+    action = wezterm.action.ActivatePaneDirection 'Up',
+  },
+  {
+    key = 'DownArrow',
+    mods = 'ALT',
+    action = wezterm.action.ActivatePaneDirection 'Down',
+  },
+};
+
+config.font = wezterm.font('JetBrains Mono', {})
+
+-- and finally, return the configuration to wezterm
+return config
