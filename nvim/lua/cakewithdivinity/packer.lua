96a1662f0e15f771879d1c7702074c4df27beaf0
hendrik.loewe <hendrikloewe3@gmail.com>
2023-05-04 20:25:11 +0200

    Add goto-preview in nvim

--- nvim/lua/cakewithdivinity/packer.lua
+++ nvim/lua/cakewithdivinity/packer.lua
@@ -8,70 +8,77 @@ return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
 
   use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.1',
-	  requires = {
-		  {'nvim-lua/plenary.nvim'}
-	  }
+    'nvim-telescope/telescope.nvim', tag = '0.1.1',
+    requires = {
+      { 'nvim-lua/plenary.nvim' }
+    }
   }
 
   use {
-	  'folke/tokyonight.nvim',
-	  config = function()
-		  vim.cmd('colorscheme tokyonight-night')
-	  end
+    'folke/tokyonight.nvim',
+    config = function()
+      vim.cmd('colorscheme tokyonight-night')
+    end
   }
 
-  use({ 'nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'}})
+  use({ 'nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' } })
   use 'nvim-treesitter/nvim-treesitter-context'
   use('theprimeagen/harpoon')
   use('tpope/vim-fugitive')
 
   use {
-	  'VonHeikemen/lsp-zero.nvim',
-	  branch = 'v1.x',
-	  requires = {
-		  -- LSP Support
-		  {'neovim/nvim-lspconfig'},             -- Required
-		  {'williamboman/mason.nvim'},           -- Optional
-		  {'williamboman/mason-lspconfig.nvim'}, -- Optional
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v1.x',
+    requires = {
+      -- LSP Support
+      { 'neovim/nvim-lspconfig' },         -- Required
+      { 'williamboman/mason.nvim' },       -- Optional
+      { 'williamboman/mason-lspconfig.nvim' }, -- Optional
 
-		  -- Autocompletion
-		  {'hrsh7th/nvim-cmp'},         -- Required
-		  {'hrsh7th/cmp-nvim-lsp'},     -- Required
-		  {'hrsh7th/cmp-buffer'},       -- Optional
-		  {'hrsh7th/cmp-path'},         -- Optional
-		  {'saadparwaiz1/cmp_luasnip'}, -- Optional
-		  {'hrsh7th/cmp-nvim-lua'},     -- Optional
+      -- Autocompletion
+      { 'hrsh7th/nvim-cmp' },     -- Required
+      { 'hrsh7th/cmp-nvim-lsp' }, -- Required
+      { 'hrsh7th/cmp-buffer' },   -- Optional
+      { 'hrsh7th/cmp-path' },     -- Optional
+      { 'saadparwaiz1/cmp_luasnip' }, -- Optional
+      { 'hrsh7th/cmp-nvim-lua' }, -- Optional
 
-		  -- Snippets
-		  {'L3MON4D3/LuaSnip'},             -- Required
-		  {'rafamadriz/friendly-snippets'}, -- Optional
-	  }
+      -- Snippets
+      { 'L3MON4D3/LuaSnip' },         -- Required
+      { 'rafamadriz/friendly-snippets' }, -- Optional
+    }
   }
 
   use {
-	  "folke/trouble.nvim",
-	  requires = "nvim-tree/nvim-web-devicons",
-	  config = function()
-		  require("trouble").setup {
-		  }
-	  end
+    "folke/trouble.nvim",
+    requires = "nvim-tree/nvim-web-devicons",
+    config = function()
+      require("trouble").setup {
+      }
+    end
   }
 
   use('WhoIsSethDaniel/toggle-lsp-diagnostics.nvim')
 
   use {
-	  'nvim-lualine/lualine.nvim',
-	  requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+    'nvim-lualine/lualine.nvim',
+    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
   }
 
   use {
-	  'lewis6991/gitsigns.nvim',
+    'lewis6991/gitsigns.nvim',
   }
 
   use {
-      'prettier/vim-prettier'
+    'prettier/vim-prettier'
   }
 
   use 'windwp/nvim-ts-autotag'
+
+  use {
+    'rmagatti/goto-preview',
+    config = function()
+      require('goto-preview').setup {}
+    end
+  }
 end)
--- nvim/lua/cakewithdivinity/remap.lua
+++ nvim/lua/cakewithdivinity/remap.lua
@@ -4,3 +4,5 @@ vim.keymap.set("n", "<leader>lq", vim.lsp.buf.code_action);
 
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+vim.keymap.set("n", "<leader>gp", require('goto-preview').goto_preview_definition)
