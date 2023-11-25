5764b58875f1dbafb93c78180c7a0c75914bd3aa
hendrik.loewe <hendrikloewe3@gmail.com>
2023-11-25 19:49:47 +0100

    Using alacritty for now :)

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,5 +1,5 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 -- This table will hold the configuration.
 local config = {}
@@ -7,64 +7,66 @@ local config = {}
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
 
 -- This is where you actually apply your config choices
 
--- For example, changing the color scheme:
-config.color_scheme = 'Tokyo Night (Gogh)'
-config.hide_tab_bar_if_only_one_tab = true
-config.enable_wayland = true
+--config.enable_wayland = false
+--config.front_end = "WebGpu"
 
-config.window_background_image = '/home/hloewe/Pictures/Wallpaper/butterfly.jpg'
+-- For example, changing the color scheme:
+config.color_scheme = "Tokyo Night (Gogh)"
+config.hide_tab_bar_if_only_one_tab = true
+
+config.window_background_image = "/home/hloewe/Pictures/Wallpaper/butterfly.jpg"
 config.window_background_image_hsb = {
-  brightness = 0.01,
-  hue = 1,
-  saturation = 1,
+	brightness = 0.01,
+	hue = 1,
+	saturation = 1,
 }
 
 config.window_padding = {
-  left = 0,
-  right = 0,
-  top = 0,
-  bottom = 0,
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
 }
 
 config.keys = {
-  {
-    key = 'e',
-    mods = 'CTRL|SHIFT',
-    action = wezterm.action.SplitHorizontal{ domain = 'CurrentPaneDomain' },
-  },
-  {
-    key = 'o',
-    mods = 'CTRL|SHIFT',
-    action = wezterm.action.SplitVertical{ domain = 'CurrentPaneDomain' },
-  },
-  {
-    key = 'LeftArrow',
-    mods = 'ALT',
-    action = wezterm.action.ActivatePaneDirection 'Left',
-  },
-  {
-    key = 'RightArrow',
-    mods = 'ALT',
-    action = wezterm.action.ActivatePaneDirection 'Right',
-  },
-  {
-    key = 'UpArrow',
-    mods = 'ALT',
-    action = wezterm.action.ActivatePaneDirection 'Up',
-  },
-  {
-    key = 'DownArrow',
-    mods = 'ALT',
-    action = wezterm.action.ActivatePaneDirection 'Down',
-  },
-};
+	{
+		key = "e",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "o",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "LeftArrow",
+		mods = "ALT",
+		action = wezterm.action.ActivatePaneDirection("Left"),
+	},
+	{
+		key = "RightArrow",
+		mods = "ALT",
+		action = wezterm.action.ActivatePaneDirection("Right"),
+	},
+	{
+		key = "UpArrow",
+		mods = "ALT",
+		action = wezterm.action.ActivatePaneDirection("Up"),
+	},
+	{
+		key = "DownArrow",
+		mods = "ALT",
+		action = wezterm.action.ActivatePaneDirection("Down"),
+	},
+}
 
-config.font = wezterm.font('JetBrains Mono', {})
+config.font = wezterm.font("Monaspace Neon", {})
 
 -- and finally, return the configuration to wezterm
 return config
